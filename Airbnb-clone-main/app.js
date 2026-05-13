42b3e03f0d7733115ed2145178c87cef1788e769
Tanisha Gaikwad <tanishagaikwad45@gmail.com>
2026-05-13 19:50:17 +0530

    Add responsive CSS for mobile and tablet

--- Airbnb-clone-main/app.js
+++ Airbnb-clone-main/app.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const tls = require('tls');
+tls.DEFAULT_MIN_VERSION = 'TLSv1.2';
+
 console.log(process.env.SECRET);
 
 const express = require('express');
@@ -19,7 +22,7 @@ const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
 
 
-const dbUrl= process.env.ATLAS_URL;
+const dbUrl = process.env.ATLAS_URL;
 
 main().then(() => {
     console.log("Connected to MongoDB");
@@ -36,68 +39,50 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-app.use(express.static
-    (path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret:process.env.SECRET,
+        secret: process.env.SECRET,
     },
-    touchAfter: 24 *3600,
+    touchAfter: 24 * 3600,
 });
 
 
-const sessionOptions ={
+const sessionOptions = {
     store,
-    secret:process.env.SECRET,
-    resave:false,
-    saveUninitialized :true,
-    cookie:{
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000 ,
-        maxAge : 7 * 24 * 60 * 60 * 1000 ,
-        httpOnly:true,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi,I am root");
-// });
-
-
-
 app.use(session(sessionOptions));
 app.use(flash());
-
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-res.locals.success = req.flash("success");
-res.locals.error = req.flash("error");
-res.locals.currUser = req.user;
-next();
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
 });
-
-// app.get("/demouser",async(req,res)=>{
-//     let fakeUser = new User({
-//         email:"student@gmail.com",
-//         username:"delta-student"
-//     });
-//    let registeredUser = await User.register(fakeUser,"helloworld");
-//    res.send(registeredUser);
-// });
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
-app.use("/",userRouter);
+app.use("/", userRouter);
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
